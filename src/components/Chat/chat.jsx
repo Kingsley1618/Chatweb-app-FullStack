be44eeb6bdc4f2ad901219bd4154c835dedab57a
Okeze Kingsley <okezekingsley18@gmail.com>
2023-10-27 07:14:36 +0100

    latest commit

--- src/components/Chat/chat.jsx
+++ src/components/Chat/chat.jsx
@@ -5,7 +5,7 @@ import Mainchat from "@/components/Chat/MainChat/chat"
 import PopupModal from "../Modal/modal"
 export default function Chat() {
   return (
-    <div className='flex-1 flex overflow-y-hidden'>
+    <div className='flex-1 flex overflow-y-hidden mt-[7vh]'>
 <Sidebar />
 <Mainchat />
 <PopupModal />
